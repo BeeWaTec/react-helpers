ee5ea01d09d2bf1642e370d8b0da878203b97ada
Andreas Ehrhardt <ehrhardt@t4ggno.com>
2023-04-04 23:36:19 +0200

    Adds "userHasRole"

--- auth0.tsx
+++ auth0.tsx
@@ -10,6 +10,18 @@ function checkIfAdmin(user: any | null){
     }
 }
 
+function userHasRole(user: any | null, role: string){
+    try {
+        if (user && user.user && user.user.pp_roles && user.user.pp_roles.includes(role)) {
+            return true;
+        }
+        return false;
+    }
+    catch (e) {
+        return false;
+    }
+}
+
 function checkIfLoggedIn(user: any | null){
     try {
         if (user || user.user) {
