6d9a0751dd86c28707b8d70867aba42aaf1d2f67
Andreas Ehrhardt <a.ehrhardt@beewatec.de>
2024-08-01 14:46:52 +0200

    Add some more features

--- helpers.ts
+++ helpers.ts
@@ -1,9 +1,8 @@
-import { useEffect } from 'react';
-import chroma from "chroma-js"
-import { v1 as uuidv1, v3 as uuidv3, v4 as uuidv4, v5 as uuidv5 } from "uuid";
+import chroma from "chroma-js";
+import { v4 as uuidv4 } from "uuid";
 
-export function getRatingPercentage(likes: number, dislikes: number): number {
-    if (likes == dislikes) {
+export function getRatingPercentage (likes: number, dislikes: number): number {
+    if (likes === dislikes) {
         return 50;
     }
     if (likes > dislikes) {
@@ -15,13 +14,13 @@ export function getRatingPercentage(likes: number, dislikes: number): number {
     return 50;
 }
 
-export function getRatingColor(rating: number): string {
+export function getRatingColor (rating: number, type: "step" | "gradient" = "step"): string {
 
     // Clamp rating to 0-100
     rating = Math.min(Math.max(rating, 0), 100);
 
     // Check if chroma is available
-    if (chroma) {
+    if (chroma !== undefined && type === "gradient") {
         return chroma.mix("#4CBBFC", "#ad0441", rating / 100, "rgb").hex();
     }
     else {
@@ -39,10 +38,48 @@ export function getRatingColor(rating: number): string {
     }
 }
 
-export function getRandomUUID({ exclude }: { exclude?: string[] } = {}): string {
+export function getRandomUUID ({ exclude }: { exclude?: string[] } = {}): string {
     let uuid: string;
     do {
         uuid = uuidv4();
     } while (exclude && exclude.includes(uuid));
     return uuid;
 }
+
+export function convertToMarkdownLinks (text: string): string {
+    const urlRegex = /(https?:\/\/[^\s]+)(?!(?:\[(.*?)\]\((.*?)\)))/g;
+    return text.replace(urlRegex, (url) => `[${url}](${url})`);
+}
+
+export function timeFromCreatedAt (createdAt: string) {
+    // Return as hh:mm (24-hour format)
+    return new Date(createdAt).toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
+}
+
+export function dateFromCreatedAt (createdAt: string) {
+    // Return as dd/mm/yyyy
+    return new Date(createdAt).toLocaleDateString(undefined, { day: '2-digit', month: '2-digit', year: 'numeric' });
+}
+
+export function isDarkColor (color: string): boolean {
+    // Check if color is dark
+    // Valid formats: #RRGGBB, #RRGGBBAA, rgb(R, G, B), rgba(R, G, B, A) or color name
+    try {
+        const rgb = chroma(color).rgb();
+        const brightness = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
+        return brightness < 128;
+    }
+    catch {
+        console.log("Invalid color format");
+        return false;
+    }
+}
+
+export function isLightColor (color: string): boolean {
+    return !isDarkColor(color);
+}
+
+export function getContrastColor (color: string): string {
+    return isDarkColor(color) ? "#fff" : "#000";
+}
+
